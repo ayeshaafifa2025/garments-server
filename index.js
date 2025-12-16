20cdb9f959e21ca6db91ba553a3dc000cd3fdad3
ayeshaafifa2025 <ayeshaafifa2025@gmail.com>
2025-12-16 16:07:39 +0600

    created user and product collection

--- index.js
+++ index.js
@@ -5,12 +5,44 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 3000
 
+const admin = require("firebase-admin");
+
+// const serviceAccount = require("./garments-client-firebase-adminsdk.json");
+
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
 // middleware
 app.use(express.json());
 app.use(cors());
 
 
+const verifyFBToken = async (req, res, next) => {
+    const token = req.headers.authorization;
+    console.log(token)
 
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    try {
+        const idToken = token.split(' ')[1];
+        const decoded = await admin.auth().verifyIdToken(idToken);
+        console.log('decoded in the token', decoded);
+        req.decoded_email = decoded.email;
+        next();
+    }
+    catch (err) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wstr9pt.mongodb.net/?appName=Cluster0`;
 
@@ -26,9 +58,47 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+    const db = client.db('garments_order_db');
+    const userCollection = db.collection('users');
+    const productCollection = db.collection('products');
+
+
+    // User related API
+
+     app.post('/users', async (req, res) => {
+            const userInfo = req.body;
+            userInfo.role = 'user';
+            userInfo.createdAt = new Date();
+            const email = userInfo.email;
+            const userExists = await userCollection.findOne({ email })
+
+            if (userExists) {
+                return res.send({ message: 'user exists' })
+            }
+
+            const result = await userCollection.insertOne(userInfo);
+          
+            res.send(result);
+        })
+
+// Product related API
+app.post('/products', async (req, res) => {
+    try {
+        const product = req.body;
+        console.log(req.body)
+        product.createdAt = new Date();
+
+        const result = await productCollection.insertOne(product);
+        res.send(result);
+    } catch (error) {
+        res.status(500).send({ message: 'Product insert failed' });
+    }
+});
+
+
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
+    await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
